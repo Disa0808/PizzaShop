cb066d140b9300000146b2b5d9f1b6281c509360
Disa0808 <levshukdenis@mail.ru>
2017-07-12 20:58:38 +0300

    Display products on index.erb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170711204357) do
+ActiveRecord::Schema.define(version: 20170711211725) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"

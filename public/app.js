7d4e9b92cdd17b9955e80ed94927dc22b036fc3f
Disa0808 <levshukdenis@mail.ru>
2017-07-13 20:28:10 +0300

    Update add_to_cart function

--- public/app.js
+++ public/app.js
@@ -1,8 +1,7 @@
-function something()
+function add_to_cart(id)
 {
-	var x = window.localStorage.getItem("bbb");
-	x = x * 1 + 1;
-	window.localStorage.setItem('bbb', x);
-	alert(x);
+  var x = window.localStorage.getItem('product_' + id);
+  x = x * 1 + 1;
+  window.localStorage.setItem('product_' + id, x);
 	
 }

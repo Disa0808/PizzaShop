6847954a1bec152f52c3b013e73fe5eca6924ab4
Disa0808 <levshukdenis@mail.ru>
2017-07-15 17:41:05 +0300

    update orders input field

--- public/app.js
+++ public/app.js
@@ -4,7 +4,7 @@ function add_to_cart(id)
   var x = window.localStorage.getItem(key);
   x = x * 1 + 1;
   window.localStorage.setItem(key, x);
-  alert('Items in your cart: ' + cart_get_number_of_items());
+  update_orders_input();
 }
 
 
@@ -45,3 +45,9 @@ function cart_get_orders()
 }
 
 
+function update_orders_input() {
+  var orders = cart_get_orders();
+  $('#orders_input').val(orders);
+
+
+}
